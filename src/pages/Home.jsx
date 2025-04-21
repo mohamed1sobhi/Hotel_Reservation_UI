1c7a85cbc127dd918b711aa4b1396b405db1df61
AtyyaElkhamy <atiaelkhamy55@gmail.com>
2025-04-21 13:38:25 +0200

    add search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,12 @@ import AllHotels from "../components/allHotels";
 import AllReviews from "../components/allReviews";
 import AllRooms from "../components/allRooms";
 import Header from "../components/Common/Header";
+import Search from "../components/SearchBar";
 function Home() {
     return (
         <div className="">
             <Header />
-            <h1 className="text-center">Welcome to Hotel Reviews</h1>
+            <Search />
             <AllHotels />
             <AllRooms />
             {/* <AllReviews /> */}
